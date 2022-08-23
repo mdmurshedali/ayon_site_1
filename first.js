e2597c92831e84eaaed50e3364efbc7ba8f703db
mdmurshedali <murshedeteruet@gmail.com>
2022-08-24 05:14:46 +0600

    1st_page

---
+++ first.js
@@ -0,0 +1,10 @@
+// function myFunction(x) {
+//     if (x.matches) { 
+//       document.getElementById("f").classList.add("h-100");
+//     } else {
+//         document.getElementById("f").classList.add("h-75");
+//     }
+//   }
+  
+//   let x = window.matchMedia("(max-width: 600px)")
+//   myFunction(x);
